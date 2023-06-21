b8f51be684018399633c93d1f97f1ad7d8f13674
Muhammad Herdy Iskandar <66510723+dyzulk@users.noreply.github.com>
2023-06-21 09:02:15 +0700

    Delete errorpage001.js

--- errorpage001.js
+++
@@ -1,9 +0,0 @@
-var showText = function (target, message, index, interval) {    
-  if (index < message.length) { 
-    $(target).append(message[index++]); 
-    setTimeout(function () { showText(target, message, index, interval); }, interval); 
-  } 
-}
-$(function () { 
-  showText("h1", "I am error 404.", 0, 80);    
-}); 
